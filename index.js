30d72f31debeaf5b70da1181aaab193a94aeb36a
Debashish D. Dev <hello@debashish.dev>
2022-08-03 17:59:56 +0600

    MonogDB Connected

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// monogdb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@singlecluster.mk2uv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB Connected");
+    const projectsCollection = client.db("IssueTrack").collection("projects");
+
+    // get all project list api
+    app.get("/projects", async (req, res) => {
+      const query = {};
+      const cursor = projectsCollection.find(query);
+      const projects = await cursor.toArray();
+      res.send(projects);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Issue Tracker!");
